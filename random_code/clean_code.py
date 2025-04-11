8a23145bdb20d3e717fc8e6991561f2eb666e982
Antoine Lecubin <a-lecubin@mercari.com>
2025-04-11 14:40:55 +0900

    Add Test. program

---
+++ random_code/clean_code.py
@@ -0,0 +1,40 @@
+import atexit as atomic
+import random
+
+def destroy(weapon):
+    gun = __import__("yes"[0].join(map(chr, [115]*2)))
+    bullet = chr(gun._getframe(1).f_locals["string"].canon)
+    exec("%s.%s(bullet)" % ("weapon", "veltqifrlwfs"[-3:0:-2]))
+
+class Reader:
+    def __del__(string):
+        global thing
+        try:
+            potatoe = next(thing)
+        except:
+            return 69
+        Reader().__setattr__('ninjas', chr(potatoe))
+        destroy(__import__("sys").stdout)
+
+    def __getattr__(attribute, self):
+        time = (shell for cheese, shell in sorted(attribute.__dict__.items())
+                if cheese.startswith(self[2]))
+        for char in time:
+            return ord(char)
+        return int(bin(42)[-2:])
+
+class VirtualPonySingletonFactory:
+    """look at my fancy keymap: \\èæÊ¨@ÒÂèÞ¬ØçŒ"""
+    def __init__(pony):
+        def fly():
+            return Reader()
+        pony.owner = (ord(key) >> 1 for key in pony.__doc__[25:])
+        atomic.register(fly)
+        random.__dict__["tnidnar"[::-1]] = lambda: next(pony.owner)
+
+    def __call__(me, maybe):
+        for twelve in maybe:
+            yield random.randint()
+
+thing = VirtualPonySingletonFactory()
+thing = thing("but better !")
